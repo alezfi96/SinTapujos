7753a366555ba11c3a54737b03d577fd82c8ed87
jchurtado363 <jchurtado363@misena.edu.co>
2022-12-14 16:19:34 -0500

    cambio y heredado en encabezado

--- src/components/EncabezadoComp.vue
+++ src/components/EncabezadoComp.vue
@@ -1,7 +1,27 @@
 <template>
+<nav>
   <section>
     <a href=""> <img src="..\assets\Logo1.png" alt="" id="logo" /></a>
   </section>
+  
+    <ul class="menu">
+
+            <li><a href="#"><b>Inicio</b></a></li>
+            <li><a href="#"><b>Quienes Somos</b></a></li>
+            <li ><a class="li2" href=""><b> Conocete y Cuidate</b></a>
+
+                <ul class="menu1">
+                    <li><a href="#"><b>Mitos y Verdades</b></a></li>
+                    <li><a href="#"><b>Mitos y Verdades</b></a></li>
+                    <li><a href="#"><b>Mitos y Verdades</b></a></li>
+                </ul>
+            </li>
+            <li><a href="#"><b> Juegos</b></a></li>
+
+            <li><a href="#"><b>Comentarios</b></a></li>
+            <li><a href="#"><b>Agenda tu Cita</b></a></li>
+    </ul>
+</nav>
 </template>
 
 <script>
@@ -11,6 +31,37 @@ export default {
 
 </script>
 <style scoped>
+.menu{
+        text-align:justify;
+        font-size: 20px;
+        height: 5vh;
+        background-color: white;
+        display: flex;
+        justify-content:space-evenly;
+        border: 2px solid black;
+    }
+    .menu1{
+        background-color: aquamarine;
+        list-style: none;
+        display: none;
+        position: relative
+    }
+    .menu li:hover > ul{
+        display: block;
+        z-index: 10;
+    }
+    li{
+        text-decoration: none;
+        display: block;
+    }
+    a{
+        text-decoration: none;
+        display: block;
+    }
+    a:hover {
+        background-color: antiquewhite;
+        color:black;
+    }
 section {
   border: solid 1px;
   background-color: rgb(154, 239, 239);
