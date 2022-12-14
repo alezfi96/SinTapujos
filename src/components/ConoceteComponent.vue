ecec6617b96ffcb4bc968027ee4f8194c868e3c6
TatianaCruz2 <tscruz3@misena.edu.co>
2022-12-14 13:52:57 -0500

    conocete y cuidate

---
+++ src/components/ConoceteComponent.vue
@@ -0,0 +1,56 @@
+<template>
+    <main>
+        <header>
+            <nav>
+                <li><a href="#">Derechos y Deberes</a></li>
+                <li><a href="#">Métodos Anticonceptivos</a></li>
+                <li><a href="#">Sexualidad Feminina</a></li>
+                <li><a href="#">Sexualidad Masculina</a></li>
+                <li><a href="#">ITS</a></li>
+            </nav>
+        </header>
+    </main>
+</template>
+<script>
+export default{
+    name:'ConoceteComponent',
+    data(){
+
+    }
+}
+</script>
+<style scoped>
+header{
+    display: flex;
+}
+img{
+
+    width: 50%;
+}
+
+nav{
+    display: flex;
+    list-style: none;
+
+}
+li{
+    display: inline-block;
+    text-align:center ;
+    padding: 10px 35px 10px 35px;
+}
+a{
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    display:block;
+    text-decoration: none;
+    padding: 10px;
+    -webkit-transition:all 500ms ease ;
+    -o-transition: all 500ms ease;
+    transition:all 500ms ease ;
+    
+}
+a:hover{
+    
+    background-color: aquamarine;
+    border-radius: 20px;
+}
+</style>
---
+++ src/views/ConoceteView.vue
@@ -0,0 +1,15 @@
+<template>
+    <main>
+        <conoceteComponent />
+    </main>
+</template>
+<script>
+import ConoceteComponent from '@/components/ConoceteComponent.vue'
+export default{
+    name:'ConoceteView',
+    components:{
+        ConoceteComponent
+
+    }
+}
+</script>
